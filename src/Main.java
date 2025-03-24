7ab47ffe8e2a1da5e24575e08ddaaf3b3659c71f
kmk0428 <kmk030428@gmail.com>
2025-03-24 15:12:41 +0900

    25.03.24 실습3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        double round = 10.0;
-        final double PI = 3.14;
-        System.out.println((int)(round * round * PI));
+        // 주어진 값들
+        double doubleValue = 2.71828;
+
+        // double에서 다른 타입으로의 형변환 결과 출력
+        System.out.println("\n=== double 형변환 ===");
+        // byte, short, int, long이 네가지는 데이터의 크기만 다른 정수형으로 소수점 뒤의 자리값은 받지 않는다.
+        //byte < short < int < long
+        System.out.println("1. double -> byte: " + (byte)doubleValue);
+        System.out.println("2. double -> short: " + (short)doubleValue);
+        System.out.println("3. double -> int: " + (int)doubleValue);
+        System.out.println("4. double -> long: " + (long)doubleValue);
+        //float는 double 같은 실수형으로 double보다 적은 소수점 뒤의 자리값을 받을 수 있다.
+        //float < double
+        System.out.println("5. double -> float: " + (float)doubleValue);
     }
 }
