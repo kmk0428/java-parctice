45f0c7259bf478cea27eccfbc84f94ddb35e5857
kmk0428 <kmk030428@gmail.com>
2025-03-24 14:30:40 +0900

    2025.03.24 수업 실습

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double round = 10.0;
+        int PI = 3;
+        System.out.println(round * round * 3.14);
+    }
+}
