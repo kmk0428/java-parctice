b34ed7f6babef4f065e103112223c7066f7a1db2
kmk0428 <kmk030428@gmail.com>
2025-03-27 14:57:26 +0900

    20250327 실습 2번

--- src/_25032403.java
+++ src/_25032403.java
@@ -1,3 +1,5 @@
+package src;
+
 public class _25032403 {
     public static void main(String[] args) {
         // 주어진 값들
@@ -5,13 +7,15 @@ public class _25032403 {
 
         // double에서 다른 타입으로의 형변환 결과 출력
         System.out.println("\n=== double 형변환 ===");
-        // byte, short, int, long이 네가지는 데이터의 크기만 다른 정수형으로 소수점 뒤의 자리값은 받지 않는다.
-        //byte < short < int < long
         System.out.println("1. double -> byte: " + (byte)doubleValue);
+        //doubleValue 변수의 값을 double -> byte로 형변환을 하여 정수형으로 전환 되어 소수점 뒷자리를 받지 안는다. 따라서 2 출력
         System.out.println("2. double -> short: " + (short)doubleValue);
+        //doubleValue 변수의 값을 double -> short 형변환을 하여 정수형으로 전환 되어 소수점 뒷자리를 받지 안는다. 따라서 2 출력
         System.out.println("3. double -> int: " + (int)doubleValue);
+        //doubleValue 변수의 값을 double -> int 형변환을 하여 정수형으로 전환 되어 소수점 뒷자리를 받지 안는다. 따라서 2 출력
         System.out.println("4. double -> long: " + (long)doubleValue);
-        //float는 doulbe 같은 실수형으로 float는 doulbe보다 적은 소수점 뒤의 자리값을 받을 수 있다.
+        //doubleValue 변수의 값을 double -> long 형변환을 하여 정수형으로 전환 되어 소수점 뒷자리를 받지 안는다. 따라서 2 출력
         System.out.println("5. double -> float: " + (float)doubleValue);
+        //float는 doulbe 같은 실수형으로 double -> float로 형변환을 하여도 소수점 뒤의 자리값을 받을 수 있다. 따라서 2.71828 출력
     }
 }
