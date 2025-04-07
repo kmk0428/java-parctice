6d0bfea5b5ce28b878d157841391bd357425ac62
kmk04 <kmk030428@gmail.com>
2025-04-07 12:45:51 +0900

    250404수정

--- Calc/project_5.java
+++ Calc/project_5.java
@@ -5,27 +5,28 @@ public class project_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //변수 선언
-        int language, english, math, science, subject = 4;
+        double language, english, math, science, score_sum, real_score;
+        int score_mean;
         //입력
         System.out.print("국어 점수를 입력: ");
-        language = sc.nextInt();
+        language = 55; //sc.nextDouble();
         System.out.print("영어 점수를 입력: ");
-        english = sc.nextInt();
+        english = 60; //sc.nextDouble();
         System.out.print("수학 점수를 입력: ");
-        math = sc.nextInt();
+        math = 80; //sc.nextDouble();
         System.out.print("과학 점수를 입력: ");
-        science = sc.nextInt();
+        science = 85; //sc.nextDouble();
 
         //정수의 합
-        int score_sum = language + english + math + science;
+        score_sum = language + english + math + science;
         System.out.println("정수 점수의 총점은 " + score_sum);
 
         //정수의 평균
-        int score_mean  = score_sum / subject;
+        score_mean  = (int)score_sum / 4;
         System.out.println("정수 점수의 평균은 " + score_mean);
 
-        //실수의 평균S
-        double real_score = (double) score_sum / subject;
+        //실수의 평균
+        real_score = score_sum / 4;
         System.out.println("실수 평균은 " + real_score);
 
         //실수의 평균과 정수의 평균이 같은가?
