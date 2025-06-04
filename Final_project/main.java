71c3e201f65b7779dfcd69186683fe242b4e1673
kmk0428 <kmk030428@gmail.com>
2025-06-05 03:26:35 +0900

    기말 과제

--- Final_project/main.java
+++ Final_project/main.java
@@ -1,7 +1,30 @@
 package Final_project;
+import java.util.Scenner;
 
 public class main {
-    public static void main(String[] args) {
+    static Scenner sc = new Scenner(System.in);
+    
+    public static void todayTime() {
+        // 요일, 시간 정보
+        
+    }
 
+    public static void currentLocation() {
+        // 구역 분리 및 현재위치
+        
+    }
+
+    public static void busStop() {
+        // 정거장 정보
+        
+    }
+
+    public static void closer() {
+        // 가까운 정보 확인
+        
+    }
+    
+    public static void main(String[] args) {
+        
     }
 }
